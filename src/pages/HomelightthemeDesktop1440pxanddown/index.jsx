eafc43f0b83ae6b90e32c8ae0a3559830124f027
Khabibullo <ad4459089@gmail.com>
2023-07-20 11:00:41 +0500

    merged new design, content from figma

--- src/pages/HomelightthemeDesktop1440pxanddown/index.jsx
+++ src/pages/HomelightthemeDesktop1440pxanddown/index.jsx
@@ -8,74 +8,31 @@ import GridPanel from "components/GridPanel";
 import Header from "components/Header";
 import Panel from "components/Panel";
 import StepsPanel from "components/StepsPanel";
+import VideoPanel from "components/VideoPanel";
 
 import { handleSectionNavigation } from "utils";
+import Functions from "components/Functions";
+import TrustedBy from "components/TrustedBy";
+import Instructions from "components/Instructions";
+import PanelReverse from "components/PanelReverse";
+import Wps from "components/Wps";
 
 const HomelightthemeDesktop1440pxanddownPage = () => {
   return (
     <>
       <div className="bg-white-A700 flex flex-col font-rubik items-center justify-start mx-auto w-auto sm:w-full md:w-full">
         <Header className="bg-white-A700 flex items-center justify-center md:px-5 py-7 w-full" />
-        <Panel className="flex flex-col gap-[120px] h-[768px] md:h-auto items-center justify-center py-[120px] w-full" />
-        <div
-          className="flex flex-col h-[768px] md:h-auto items-center justify-center py-[120px] w-full"
-          id="block1"
-        >
-          <div className="flex md:flex-col flex-row gap-7 items-start justify-start max-w-[1080px] mx-auto md:px-5 w-full">
-            <div className="flex md:flex-1 flex-col items-center justify-start w-[49%] md:w-full">
-              <Img
-                className="h-[480px] md:h-auto object-cover rounded-bl-[40px] rounded-br-[40px] w-full"
-                src="images/img_homestationinstalled.png"
-                alt="homestationinst_One"
-              />
-            </div>
-            <div className="flex flex-1 flex-col gap-7 h-full items-start justify-center py-7 w-full">
-              <Text
-                className="leading-[44.00px] max-w-[526px] md:max-w-full text-4xl sm:text-[32px] md:text-[34px] text-black-900"
-                size="txtRubikSemiBold36"
-              >
-                Зарядные устройства для дома
-              </Text>
-              <div className="flex flex-col gap-5 items-start justify-start w-full">
-                <Text
-                  className="leading-[24.00px] max-w-[526px] md:max-w-full text-base text-black-900 tracking-[0.16px]"
-                  size="txtRubikRegular16"
-                >
-                  Заряжайте быстрее с нашими домашними зарядными устройствами
-                  для электромобилей, которые просты в установке, имеют
-                  интеллектуальные функции, прочны и надежны и могут заряжать
-                  любые электрические транспортные средства.
-                </Text>
-                <Text
-                  className="text-base text-black-900 tracking-[0.16px] w-full"
-                  size="txtRubikRegular16"
-                >
-                  Установка + поддержка включены.
-                </Text>
-              </div>
-              <div className="flex flex-row font-inter gap-3 items-center justify-start w-auto">
-                <Img
-                  className="h-5 w-5"
-                  src="images/img_refresh.svg"
-                  alt="refresh_One"
-                />
-                <Text
-                  className="text-light_green-700 text-xl w-auto"
-                  size="txtInterSemiBold20"
-                >
-                  Узнать больше
-                </Text>
-              </div>
-            </div>
-          </div>
-        </div>
-        <GridPanel
-          className="bg-gray-50 flex flex-col gap-10 items-center justify-start max-w-[1440px] pb-[120px] pt-[60px] w-full"
+        <VideoPanel className="flex flex-col items-center justify-center sm:m-[] sm:mb-[] sm:ml-[] sm:mr-[] sm:mt-[] sm:p-[] pt-16 sm:px-5 w-full" />
+        <TrustedBy className="flex flex-col gap-8 items-center justify-start pb-16 pt-32 w-full" />
+        <Panel className="bg-gray-50 flex flex-col items-center justify-center sm:p-[] pb-32 pt-16 sm:px-5 rounded-tl-[128px] rounded-tr-[128px] w-full" />
+        <Functions
+          className="bg-gray-50 flex flex-col gap-8 items-center justify-start sm:p-[] pb-32 sm:px-5 w-full"
           id="block2"
         />
-        <StepsPanel className="bg-gray-50 flex flex-col gap-10 items-center justify-start p-[60px] md:px-10 sm:px-5 w-full" />
-        <Panel className="flex flex-col gap-[120px] h-[768px] md:h-auto items-center justify-center py-[120px] w-full" />
-        <Faq className="flex flex-col gap-10 items-center justify-center pb-[120px] pt-[60px] md:px-10 sm:px-5 px-[60px] w-full" />
+        <Instructions className="bg-gray-50 flex flex-col gap-8 items-center justify-center sm:p-[] pb-16 sm:px-5 rounded-bl-[128px] rounded-br-[128px] w-full" />
+        <PanelReverse className="flex flex-col gap-[120px] items-center justify-center sm:p-[] pb-32 pt-[65px] sm:px-5 w-full" />
+        <Faq className="flex flex-col gap-8 items-center justify-center sm:p-[] pb-32 sm:px-5 w-full" />
+        <Wps className="flex flex-col gap-8 items-center justify-center sm:p-[] pb-32 sm:px-5 w-full" />
         <Advantages className="flex flex-col gap-10 items-center justify-center pb-[120px] pt-[60px] md:px-10 sm:px-5 px-[60px] w-full" />
         <Footer className="flex gap-10 items-center justify-center md:px-5 py-[60px] w-full" />
       </div>
