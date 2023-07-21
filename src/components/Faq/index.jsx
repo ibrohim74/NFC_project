a3574f5ce8c410aaaa69afb74ee7273a1e549c4b
Khabibullo <ad4459089@gmail.com>
2023-07-21 15:06:15 +0500

    working Faq section

--- src/components/Faq/index.jsx
+++ src/components/Faq/index.jsx
@@ -11,6 +11,12 @@ const Faq = (props) => {
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit",
     },
+    {
+      question:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
   ];
 
   return (
@@ -25,37 +31,14 @@ const Faq = (props) => {
           </Text>
         </div>
         <div className="flex flex-col gap-8 items-start justify-start max-w-[766px] w-full">
-          <Question
-            className="bg-gray-100 flex sm:flex-col flex-row gap-8 items-center justify-center p-8 sm:px-5 rounded-[28px] w-full"
-            faq={faqs[0]}
-          />
-          <div className="bg-gray-100 flex sm:flex-col flex-row gap-8 items-center justify-center p-8 sm:px-5 rounded-[28px] w-full">
-            <Text
-              className="flex-1 text-base text-black-900 tracking-[0.16px] w-auto"
-              size="txtRubikRegular16"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit?
-            </Text>
-            <Img
-              className="h-5 w-5"
-              src="images/img_refresh.svg"
-              alt="refresh"
+          {faqs.map((faq, index) => (
+            <Question
+              // deleted this:  gap-8
+              className="bg-gray-100 flex flex-col items-center justify-center p-8 sm:px-5 rounded-[28px] w-full"
+              faq={faq}
+              key={index}
             />
-          </div>
-          <div className="bg-gray-100 flex md:flex-col flex-row gap-8 items-center justify-center p-8 sm:px-5 rounded-[28px] w-full">
-            <Text
-              className="flex-1 leading-[24.00px] max-w-[650px] md:max-w-full text-base text-black-900 tracking-[0.16px]"
-              size="txtRubikRegular16"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Text>
-            <Img
-              className="h-5 w-5"
-              src="images/img_refresh.svg"
-              alt="refresh_One"
-            />
-          </div>
+          ))}
         </div>
       </div>
     </>
--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,20 +1,49 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "../../utils/faq.js";
 import { Img, Text } from "components";
 
 const Question = (props) => {
   const { question, answer } = { ...props.faq };
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  const handleClick = () => {
+    setShowAnswer(!showAnswer);
+  };
+
   return (
     <>
-      <div className={props.className} onClick={props.onClick} id={props.id}>
-        <Text
-          className="flex-1 text-base text-black-900 tracking-[0.16px] w-auto"
-          size="txtRubikRegular16"
+      <div
+        className={`${props.className} ${
+          showAnswer ? "cursor-zoom-out" : "cursor-zoom-in"
+        }`}
+        onClick={handleClick}
+        id={props.id}
+      >
+        <div className="flex sm:flex-col flex-row gap-8 items-center justify-center  sm:px-5 rounded-[28px] w-full">
+          <Text
+            className="flex-1 text-base text-black-900 tracking-[0.16px] w-auto"
+            size="txtRubikRegular16"
+          >
+            {question}
+          </Text>
+          <Img className="h-5 w-5" src="images/img_refresh.svg" alt="refresh" />
+        </div>
+        {/* The answer container */}
+        <div
+          className={`answer-container ${showAnswer ? "show" : ""}`}
+          style={{
+            maxHeight: showAnswer ? "1000px" : "0",
+            overflow: "hidden",
+            transition: "max-height 0.5s ease-in-out",
+          }}
         >
-          {question}
-        </Text>
-        <Img className="h-5 w-5" src="images/img_refresh.svg" alt="refresh" />
+          <Text
+            className="flex-1 text-base text-black-900 tracking-[0.16px] w-auto"
+            size="txtRubikRegular16"
+          >
+            {answer}
+          </Text>
+        </div>
       </div>
     </>
   );
@@ -23,3 +52,71 @@ const Question = (props) => {
 Question.defaultProps = {};
 
 export default Question;
+
+// import React, { useState } from "react";
+// import "../../utils/faq.js";
+// import { Img, Text } from "components";
+
+// const Question = (props) => {
+//   const { question, answer } = { ...props.faq };
+//   const [showAnswer, setShowAnswer] = useState(false);
+
+//   const handleClick = () => {
+//     setShowAnswer(!showAnswer);
+//   };
+
+//   return (
+//     <>
+//       <div
+//         className={props.className}
+//         onClick={handleClick}
+//         id={props.id}
+//         style={{
+//           cursor: "pointer",
+//           display: "flex",
+//           alignItems: "center",
+//           justifyContent: "space-between",
+//           transition: "all 0.3s ease-in-out", // Adding the transition here
+//         }}
+//       >
+//         <Text
+//           className="flex-1 text-base text-black-900 tracking-[0.16px] w-auto"
+//           size="txtRubikRegular16"
+//         >
+//           {showAnswer ? answer : question}
+//         </Text>
+//         <Img className="h-5 w-5" src="images/img_refresh.svg" alt="refresh" />
+//       </div>
+//     </>
+//   );
+// };
+
+// Question.defaultProps = {};
+
+// export default Question;
+
+// import React from "react";
+
+// import "../../utils/faq.js";
+// import { Img, Text } from "components";
+
+// const Question = (props) => {
+//   const { question, answer } = { ...props.faq };
+//   return (
+//     <>
+//       <div className={props.className} onClick={props.onClick} id={props.id}>
+//         <Text
+//           className="flex-1 text-base text-black-900 tracking-[0.16px] w-auto"
+//           size="txtRubikRegular16"
+//         >
+//           {question}
+//         </Text>
+//         <Img className="h-5 w-5" src="images/img_refresh.svg" alt="refresh" />
+//       </div>
+//     </>
+//   );
+// };
+
+// Question.defaultProps = {};
+
+// export default Question;
